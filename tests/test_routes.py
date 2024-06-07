9c8e40e4614d5d787428bee93b50d344999ac1f0
Robin Andrew <robinrew@gmail.com>
2024-06-07 13:17:10 +0100

    Added failing index test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,3 +1,9 @@
 def test_image_cache(client):
     response = client.get("/rs:200:200/s:4/test_image.jpeg")
     assert response.status_code == 200
+
+
+def test_index(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Simple Image Cache Server Running" in response.data
