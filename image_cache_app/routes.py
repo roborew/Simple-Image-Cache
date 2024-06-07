3c9184be6714bbf8169a287856546b61cb6e30b4
Robin Andrew <robinrew@gmail.com>
2024-06-07 13:17:45 +0100

    Passing Index test

--- image_cache_app/routes.py
+++ image_cache_app/routes.py
@@ -3,6 +3,11 @@ from flask import Blueprint
 bp = Blueprint("routes", __name__)
 
 
+@bp.route("/")
+def index():
+    return "Simple Image Cache Server Running"
+
+
 @bp.route("/<path:url>")
 def image_cache(url):
     return f"You entered: {url}"
